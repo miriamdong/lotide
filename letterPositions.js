b3c095922c2ca337dc77fdb384b7dba813ac06c5
miriamdong <miriamdong@gmai.com>
2021-03-03 21:07:41 +0000

    add letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+const eqArrays = function(a, b) {
+  let result;
+  if (a.length = b.length) {
+    for (let i = 0; i < a.length; i++) {
+      result = a[i] === b[i] ? true : false;
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  (eqArrays(actual, expected)) ? console.log('passed'): console.log('failed');
+};
+
+const letterPositions = function(str) {
+  const results = {};
+  for (let i = 0; i < str.length; i++) {
+    // console.log(letter);
+    const letter = str[i];
+    if (letter === ' ') {
+      continue;
+    }
+
+    if (!results[letter]) {
+      results[letter] = [];
+    }
+
+    results[letter].push(i);
+
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").e, [1]);
