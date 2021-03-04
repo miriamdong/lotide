a77baae62f2497f797396c72e73626e6faf8ae8e
miriamdong <miriamdong@gmai.com>
2021-03-04 21:42:59 +0000

    add map

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
